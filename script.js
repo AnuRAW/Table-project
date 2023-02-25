61c3d1f0b97aa771fde09d3a64883e5d2a74c1b7
Anurag Rawat <117522825+AnuRAW@users.noreply.github.com>
2023-02-25 17:22:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+let tableData = [
+    {id:1, student_name:"Avi", student_roll:"2020500", subject:"english", marks:95, markedBy:"teacher"},
+    {id:2, student_name:"Anu", student_roll:"20205001", subject:"english",marks:100,markedBy:"teacher"},
+    {id:3, student_name:"Abhi", student_roll:"20205002", subject:"english",marks:90,markedBy:"teacher"},
+  ];
+  console.log(tableData)
